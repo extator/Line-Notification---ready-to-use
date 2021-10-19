282ebfabcc6f7842ee18dbeb56375575e77d3d62
Mongkol N <81619586+extator@users.noreply.github.com>
2021-10-19 11:38:45 +0700

    Create config.js

---
+++ functions/config.js
@@ -0,0 +1,10 @@
+const lineCredential = {
+    ACCESS_TOKEN: '*line massage api token*'
+}
+const firebaseCredential = {
+    DATABASE_URL: '*firebase database url*'
+}
+module.exports = {
+    lineCredential,
+    firebaseCredential
+}
